43b9213622a287072b89bd710329b5dc66f6caa7
androidblog <androidblog@yandex.ru>
2023-03-15 12:26:09 +0400

    implement sumbit form

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,28 @@
-import { useState } from 'react'
-import Rating from './components/Rating'
-import Thanks from './components/Thanks';
+import { useState } from "react";
+import Rating from "./components/Rating";
+import Thanks from "./components/Thanks";
 
 function App() {
   const [count, setCount] = useState(0);
-
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const formJson = Object.fromEntries(formData.entries());
+    setCount(Number(formJson.rating));
+  };
   return (
     <div className="rating-container">
-      {/* <Rating /> */}
-<Thanks rating={count}/>
-
+      {count === 0 ? (
+        <Rating
+          handleSubmit={handleSubmit}
+          rating={count}
+          setRating={setCount}
+        />
+      ) : (
+        <Thanks rating={count} />
+      )}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default function Rating() {
+export default function Rating({ handleSubmit }) {
   return (
     <>
       <div className="round-container">
@@ -11,25 +11,25 @@ export default function Rating() {
         Please let us know how we did with your support request. All feedback is
         appreciated to help us improve our offering!
       </p>
-      <form className="rating-form">
+      <form onSubmit={handleSubmit} className="rating-form">
         <div className="rating-buttons">
-          <input type="radio" name="rating" id="r1" />
+          <input type="radio" name="rating" id="r1" value="1" />
           <label htmlFor="r1" className="round-container">
             1
           </label>
-          <input type="radio" name="rating" id="r2" />
+          <input type="radio" name="rating" id="r2" value="2" />
           <label htmlFor="r2" className="round-container">
             2
           </label>
-          <input type="radio" name="rating" id="r3" />
+          <input type="radio" name="rating" id="r3" value="3" />
           <label htmlFor="r3" className="round-container">
             3
           </label>
-          <input type="radio" name="rating" id="r4" />
+          <input type="radio" name="rating" id="r4" value="4" />
           <label htmlFor="r4" className="round-container">
             4
           </label>
-          <input type="radio" name="rating" id="r5" />
+          <input type="radio" name="rating" id="r5" value="5" />
           <label htmlFor="r5" className="round-container">
             5
           </label>
@@ -38,6 +38,6 @@ export default function Rating() {
           Submit
         </button>
       </form>
-      </>
+    </>
   );
 }
